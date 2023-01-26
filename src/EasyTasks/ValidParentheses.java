43e486046ac12baa4ee78112cb67920e0c81d157
Yury Kulakov <sav1orxp@gmail.com>
2023-01-26 16:28:32 +0300

    added new solutions

--- src/EasyTasks/ValidParentheses.java
+++ src/EasyTasks/ValidParentheses.java
@@ -6,20 +6,39 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Stack;
 
 public class ValidParentheses {
     public static void main(String[] args) {
-        System.out.println(isValid("{{[}]}"));
+        System.out.println(isValid("{[]}"));
     }
 
-    public static   boolean isValid(String s) {
+    public static boolean isValid(String s) {
 
-        while (s.contains("{}") || s.contains("()") || s.contains("[]")) {
-            s = s.replace( "{}", "");
-            s = s.replace("()", "");
-            s = s.replace ("[]", "");
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.size() > 0) {
+                    char temp = stack.pop();
+                    if ((temp == '(' && c != ')') || (temp == '{' && c != '}') || (temp == '[' && c != ']')) {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
         }
-        return s.isEmpty();
+        return stack.size() == 0 ? true : false;
+
+//        while (s.contains("{}") || s.contains("()") || s.contains("[]")) {
+//            s = s.replace( "{}", "");
+//            s = s.replace("()", "");
+//            s = s.replace ("[]", "");
+//        }
+//        return s.isEmpty();
 //        boolean what = false;
 //        List<String> list = new ArrayList();
 //        Collections.addAll(list, s.split(""));
