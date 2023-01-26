6f442ed6692610083954cb0afb30ff071fcf8bdc
Yury Kulakov <sav1orxp@gmail.com>
2023-01-26 21:31:49 +0300

    added new solutions

--- src/EasyTasks/MergeTwoSortedLists.java
+++ src/EasyTasks/MergeTwoSortedLists.java
@@ -7,52 +7,47 @@ import java.util.List;
 public class MergeTwoSortedLists {
 
     public static void main(String[] args) {
-        ListNode1 head=new  ListNode1();
-        ListNode1 head1=new ListNode1();
-        ListNode1 head2=new ListNode1();
-        ListNode1 head3=new ListNode1();
-        ListNode1 head4=new ListNode1();
-        ListNode1 head5=new ListNode1();
-        head.val=1;
-        head1.val=2;
-        head2.val=4;
-        head3.val=1;
-        head4.val=3;
-        head5.val=4;
-        head.next=head1;
-        head1.next=head2;
-        head3.next=head4;
-        head4.next=head5;
+        ListNode1 head = new ListNode1();
+        ListNode1 head1 = new ListNode1();
+        ListNode1 head2 = new ListNode1();
+        ListNode1 head3 = new ListNode1();
+        ListNode1 head4 = new ListNode1();
+        ListNode1 head5 = new ListNode1();
+        head.val = 1;
+        head1.val = 2;
+        head2.val = 4;
+        head3.val = 1;
+        head4.val = 3;
+        head5.val = 4;
+        head.next = head1;
+        head1.next = head2;
+        head3.next = head4;
+        head4.next = head5;
         List list = new ArrayList();
-        list.add(mergeTwoLists(head,head3));
+        list.add(mergeTwoLists(head, head3));
         System.out.println(list);
     }
 
     public static ListNode1 mergeTwoLists(ListNode1 list1, ListNode1 list2) {
-        ListNode1 head = new ListNode1(-1);
+        ListNode1 head = new ListNode1(0);
         ListNode1 listNode1 = head;
 
-        while(list1!=null&&list2!=null){
-            if(list1.val<=list2.val){
-                listNode1.next=new ListNode1(list1.val);
-                list1=list1.next;
-            }else{
-                listNode1.next = new ListNode1(list2.val);
-                list2=list2.next;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                listNode1.next = list1;
+                list1 = list1.next;
+            } else {
+                listNode1.next = list2;
+                list2 = list2.next;
             }
             listNode1 = listNode1.next;
         }
 
-        while (list1!=null){
-            listNode1.next=new ListNode1(list1.val);
-            list1=list1.next;
-            listNode1=listNode1.next;
+        if (list1 != null) {
+            listNode1.next = list1;
         }
-
-        while (list2!=null){
-            listNode1.next=new ListNode1(list2.val);
-            list2=list2.next;
-            listNode1=listNode1.next;
+        if (list2 != null) {
+            listNode1.next = list2;
         }
         return head.next;
     }
