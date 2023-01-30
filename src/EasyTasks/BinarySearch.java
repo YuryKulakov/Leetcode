db34e6a6510c1dda662afca46471fdca66ce8a1e
Yury Kulakov <sav1orxp@gmail.com>
2023-01-30 16:41:13 +0300

    added new solutions

--- src/EasyTasks/BinarySearch.java
+++ src/EasyTasks/BinarySearch.java
@@ -3,25 +3,25 @@ package EasyTasks;
 public class BinarySearch {
     public static void main(String[] args) {
         int[] nums={-1,0,3,5,9,12};
-        System.out.println(search(nums,9));
+        System.out.println(search(nums,89));
     }
 
     public static int search(int[] nums, int target) {
-       int leftIndex=0;
-       int rightIndex= nums.length-1;
+        int left = 0, right = nums.length;
 
-       while (leftIndex<=rightIndex){
-           int mid = ((leftIndex+rightIndex)/2);
-           if(nums[mid]==target){
-               return mid;
-           }else if(nums[mid]<target){
-               leftIndex = mid+1;
-           }else if(nums[mid]>target){
-               rightIndex = mid-1;
-           }else {
-               break;
-           }
-       }
-       return -1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        if (left > 0 && nums[left - 1] == target) {
+            return left - 1;
+        } else {
+            return -1;
+        }
     }
 }
