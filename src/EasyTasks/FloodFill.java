cf8ef6139d8399d05f23139cf92c7ef0891733a4
Yury Kulakov <sav1orxp@gmail.com>
2023-01-30 22:22:58 +0300

    added new solutions

--- src/EasyTasks/FloodFill.java
+++ src/EasyTasks/FloodFill.java
@@ -4,27 +4,38 @@ import java.util.Arrays;
 
 public class FloodFill {
     public static void main(String[] args) {
-        int [][] image = {{1,1,1},{1,1,0},{1,0,1}};
+        int[][] image = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
         int sr = 1, sc = 1, color = 2;
-        floodFill(image,sr,sc,color);
+        floodFill(image, sr, sc, color);
         for (int i = 0; i < image.length; i++) {
             System.out.println(Arrays.toString(image[i]));
 
         }
     }
 
-    public static int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        int color = image[sr][sc];
-        if (color != newColor) dfs(image, sr, sc, color, newColor);
+    public static int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int originalColor = image[sr][sc];
+        if (originalColor != color) {
+            dfs(image, sr, sc, originalColor, color);
+        }
         return image;
     }
-    public static void dfs(int[][] image, int r, int c, int color, int newColor) {
-        if (image[r][c] == color) {
-            image[r][c] = newColor;
-            if (r >= 1) {dfs(image, r-1, c, color, newColor);}
-            if (c >= 1) {dfs(image, r, c-1, color, newColor);}
-            if (r+1 < image.length) {dfs(image, r+1, c, color, newColor);}
-            if (c+1 < image[0].length) {dfs(image, r, c+1, color, newColor);}
+
+    public static void dfs(int[][] image, int r, int c, int originalColor, int color) {
+        if (image[r][c] == originalColor) {
+            image[r][c] = color;
+            if (r >= 1) {
+                dfs(image, r - 1, c, originalColor, color);
+            }
+            if (c >= 1) {
+                dfs(image, r, c - 1, originalColor, color);
+            }
+            if (r + 1 < image.length) {
+                dfs(image, r + 1, c, originalColor, color);
+            }
+            if (c + 1 < image[0].length) {
+                dfs(image, r, c + 1, originalColor, color);
+            }
         }
     }
 }
