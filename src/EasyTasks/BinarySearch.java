ff44fe389be4ce5442ff67b5c8d397a074c8aa82
Yury Kulakov <sav1orxp@gmail.com>
2023-01-30 15:14:26 +0300

    added new solutions

---
+++ src/EasyTasks/BinarySearch.java
@@ -0,0 +1,17 @@
+package EasyTasks;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] nums={-1,0,3,5,9,12};
+        System.out.println(search(nums,89));
+    }
+
+    public static int search(int[] nums, int target) {
+        for(int i=0;i< nums.length;i++){
+            if(nums[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
