a7073a73bd4149197933b85cb24cdc83adee1de9
Yury Kulakov <sav1orxp@gmail.com>
2023-02-01 18:19:35 +0300

    added new solutions

---
+++ src/EasyTasks/FirstBadVersion.java
@@ -0,0 +1,31 @@
+package EasyTasks;
+
+public class FirstBadVersion {
+    final static int BAD = 4;
+
+    public static void main(String[] args) {
+        int answer = firstBadVersion(13);
+        System.out.println(answer);
+    }
+
+    public static int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    public static boolean isBadVersion(int bad) {
+        if (bad >= BAD) {
+            return true;
+        }
+        return false;
+    }
+}
