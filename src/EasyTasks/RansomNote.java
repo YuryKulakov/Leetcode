f7a45bc9995b13230e4d36442641410c19a94054
Yury Kulakov <sav1orxp@gmail.com>
2023-02-02 12:43:19 +0300

    added new solutions

--- src/EasyTasks/RansomNote.java
+++ src/EasyTasks/RansomNote.java
@@ -3,28 +3,21 @@ package EasyTasks;
 public class RansomNote {
 
     public static void main(String[] args) {
-        String ransomNote = "aab", magazine = "bab";
+        String ransomNote = "aab", magazine = "bba";
         boolean result = canConstruct(ransomNote, magazine);
         System.out.println(result);
     }
 
     public static boolean canConstruct(String ransomNote, String magazine) {
-        if (magazine.contains(ransomNote)) {
-            return true;
-        }
-        char[] ransom = ransomNote.toLowerCase().toCharArray();
-        char[] magaz = magazine.toLowerCase().toCharArray();
+        if (ransomNote.length() > magazine.length()) return false;
+        int[] alphabets_counter = new int[26];
 
-        for (int i = 0; i < ransom.length; i++) {
-            for (int j = 0; j < magaz.length; j++) {
-                if (ransom[i] == magaz[j]) {
-                    magaz[j]=' ';
-                    break;
-                }
-                if (j == magaz.length - 1 && ransom[i] != magaz[j]) {
-                    return false;
-                }
-            }
+        for (char c : magazine.toCharArray())
+            alphabets_counter[c-'a']++;
+
+        for (char c : ransomNote.toCharArray()){
+            if (alphabets_counter[c-'a'] == 0) return false;
+            alphabets_counter[c-'a']--;
         }
         return true;
     }
