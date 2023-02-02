be8ead0acb5dad13557d595d09e22391133b97dd
Yury Kulakov <sav1orxp@gmail.com>
2023-02-02 12:21:53 +0300

    added new solutions

--- src/EasyTasks/RansomNote.java
+++ src/EasyTasks/RansomNote.java
@@ -3,7 +3,7 @@ package EasyTasks;
 public class RansomNote {
 
     public static void main(String[] args) {
-        String ransomNote = "aab", magazine = "baa";
+        String ransomNote = "aab", magazine = "bab";
         boolean result = canConstruct(ransomNote, magazine);
         System.out.println(result);
     }
@@ -14,12 +14,10 @@ public class RansomNote {
         }
         char[] ransom = ransomNote.toLowerCase().toCharArray();
         char[] magaz = magazine.toLowerCase().toCharArray();
-        char[] result = new char[ransomNote.length()];
 
         for (int i = 0; i < ransom.length; i++) {
             for (int j = 0; j < magaz.length; j++) {
                 if (ransom[i] == magaz[j]) {
-                    result[i] = ransom[i];
                     magaz[j]=' ';
                     break;
                 }
